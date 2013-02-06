4dcb112e3220ee6b39f98b5dec03b41c978d0f34
Jonathan Martin <me@nybblr.com>
2013-02-06 14:03:15 -0500

    Forgot some dependencies. Added notes on which are standard/which ones need to be installed through golems.

--- dartsim.rb
+++ dartsim.rb
@@ -9,7 +9,13 @@ class Dartsim < Formula
 		:revision => "ca3042ed7f0f19895eec626edf1b72b93392dd35"
 	version "2.2.1"
 
+	# Standard (on homebrew)
   depends_on 'cmake' => :build
+  depends_on 'flann' => :build
+  depends_on 'boost' => :build
+  depends_on 'eigen' => :build
+
+	# Non-standard (install through golems)
   depends_on 'fcl' => :build
   depends_on 'libccd' => :build
   depends_on 'assimp3' => :build
