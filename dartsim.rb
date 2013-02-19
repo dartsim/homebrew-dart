3ef1ce798272f09ce3a10e1fbc6f97b60a9cdfa6
Jonathan Martin <me@nybblr.com>
2013-02-19 13:11:41 -0500

    Switching to tags; bumped to 2.3.

--- dartsim.rb
+++ dartsim.rb
@@ -1,34 +1,34 @@
 require 'formula'
 
 class Dartsim < Formula
-  homepage 'http://dart.golems.org'
-  head 'https://github.com/golems/dart.git',
+	homepage 'http://dart.golems.org'
+	head 'https://github.com/golems/dart.git',
 		:using => :git
-  url 'https://github.com/golems/dart.git',
+	url 'https://github.com/golems/dart.git',
 		:using => :git,
-		:revision => "ca3042ed7f0f19895eec626edf1b72b93392dd35"
-	version "2.2.1"
+		:tag => "2.3"
+	version "2.3"
 
 	# Standard (on homebrew)
-  depends_on 'cmake' => :build
-  depends_on 'flann' => :build
-  depends_on 'boost' => :build
-  depends_on 'eigen' => :build
+	depends_on 'cmake' => :build
+	depends_on 'flann' => :build
+	depends_on 'boost' => :build
+	depends_on 'eigen' => :build
 
 	# Non-standard (install through golems)
-  depends_on 'fcl' => :build
-  depends_on 'libccd' => :build
-  depends_on 'assimp3' => :build
-  depends_on 'tinyxml' => :build
-  depends_on 'tinyxml2' => :build
-  depends_on 'gtest' => :build
+	depends_on 'fcl' => :build
+	depends_on 'libccd' => :build
+	depends_on 'assimp3' => :build
+	depends_on 'tinyxml' => :build
+	depends_on 'tinyxml2' => :build
+	depends_on 'gtest' => :build
 
-  def install
-    system "cmake", ".", *std_cmake_args
-    system "make install" # if this fails, try separate make/make install steps
-  end
+	def install
+		system "cmake", ".", *std_cmake_args
+		system "make install" # if this fails, try separate make/make install steps
+	end
 
-  def test
-    system "false"
-  end
+	def test
+		system "false"
+	end
 end
--- grip.rb
+++ grip.rb
@@ -1,24 +1,24 @@
 require 'formula'
 
 class Grip < Formula
-  homepage 'http://dart.golems.org'
-  head 'https://github.com/golems/grip.git',
+	homepage 'http://dart.golems.org'
+	head 'https://github.com/golems/grip.git',
 		:using => :git
-  url 'https://github.com/golems/grip.git',
+	url 'https://github.com/golems/grip.git',
 		:using => :git,
-		:revision => "8c8ccb960e83e67060a3b8ad390b9327634d0802"
-	version "2.2.1"
+		:tag => "2.3"
+	version "2.3"
 
-  depends_on 'cmake' => :build
-  depends_on 'dartsim' => :build
-  depends_on 'wxmac' => :build
+	depends_on 'cmake' => :build
+	depends_on 'dartsim' => :build
+	depends_on 'wxmac' => :build
 
-  def install
-    system "cmake", ".", *std_cmake_args
-    system "make install" # if this fails, try separate make/make install steps
-  end
+	def install
+		system "cmake", ".", *std_cmake_args
+		system "make install" # if this fails, try separate make/make install steps
+	end
 
-  def test
-    system "false"
-  end
+	def test
+		system "false"
+	end
 end
