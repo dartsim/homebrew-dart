c0d2a1117b43ee7ebdafb8ea3fc03a63badbe767
Steven Peters <scpeters@osrfoundation.org>
2015-01-19 09:41:37 -0800

    fcl 0.3.2, add optional flann dependency

--- fcl.rb
+++ fcl.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Fcl < Formula
   homepage 'http://gamma.cs.unc.edu/FCL'
-  url 'https://github.com/flexible-collision-library/fcl/archive/0.2.9.tar.gz'
-  sha1 'ee45a4d7a2d3c56223929b452463035b3d8af1fb'
+  url 'https://github.com/flexible-collision-library/fcl/archive/0.3.2.tar.gz'
+  sha1 '67c4a3c9b2c9c829cae3e08a374fc6aa23f3f4b1'
   head 'https://github.com/flexible-collision-library/fcl.git'
 
   depends_on 'boost'
   depends_on 'cmake' => :build
+  depends_on 'homebrew/science/flann' => :optional
   depends_on 'homebrew/science/libccd'
 
   def install
