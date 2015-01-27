7dd32bd33941937e2912e1b33fdb3194977a9725
Jeongseok Lee <jslee02@gmail.com>
2015-01-27 15:21:15 -0500

    Create dartsim4.2.rb

---
+++ dartsim4.2.rb
@@ -0,0 +1,29 @@
+require 'formula'
+
+class Dartsim < Formula
+  homepage 'http://dartsim.github.io'
+  url 'https://github.com/dartsim/dart/archive/v4.2.1.tar.gz'
+  sha1 'f65bac70fd39a122c5f84b4269038a230f1d9fdf'
+  head 'https://github.com/dartsim/dart.git', :branch => 'release-4.2'
+
+  option 'core-only', 'Build dart-core only'
+
+  depends_on 'cmake' => :build
+  depends_on 'eigen' => :build
+
+  depends_on 'assimp'
+  depends_on 'boost'
+  depends_on 'fcl'
+  depends_on 'flann' unless build.include? 'core-only'
+  depends_on 'homebrew/science/libccd'
+  depends_on 'tinyxml' unless build.include? 'core-only'
+  depends_on 'tinyxml2' unless build.include? 'core-only'
+  depends_on 'ros/deps/urdfdom' unless build.include? 'core-only'
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_CORE_ONLY=True" if build.include? 'core-only'
+    system "cmake", ".", *cmake_args
+    system "make install"
+  end
+end
