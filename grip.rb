8c4b2a2fbb2108d06cb114f8ca8e10e66bd7825c
Jeongseok Lee <jslee02@gmail.com>
2015-10-18 09:40:26 -0400

    grip: more style update

--- grip.rb
+++ grip.rb
@@ -1,15 +1,13 @@
-require 'formula'
-
 class Grip < Formula
-  homepage 'http://dart.golems.org'
-  head 'https://github.com/dartsim/grip.git'
-  url 'https://github.com/dartsim/grip.git',
+  homepage "http://dart.golems.org"
+  head "https://github.com/dartsim/grip.git"
+  url "https://github.com/dartsim/grip.git",
     :revision => "21ed8a6c3ae45196ee879f1674ae1ebc67f36d0d"
   version "2.5"
 
-  depends_on 'cmake' => :build
-  depends_on 'dartsim/dart/dartsim3' => :build
-  depends_on 'wxmac' => :build
+  depends_on "cmake" => :build
+  depends_on "dartsim/dart/dartsim3" => :build
+  depends_on "wxmac" => :build
 
   def install
     system "cmake", ".", *std_cmake_args
