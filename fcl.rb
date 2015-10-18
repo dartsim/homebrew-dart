c48dc81d99f2194b5eb96ba0d0d79bacf6c41945
Jeongseok Lee <jslee02@gmail.com>
2015-10-18 09:40:14 -0400

    fcl: more style update

--- fcl.rb
+++ fcl.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class Fcl < Formula
-  homepage 'http://gamma.cs.unc.edu/FCL'
-  url 'https://github.com/flexible-collision-library/fcl/archive/0.3.2.tar.gz'
-  sha256 'cf914f85b32cf8b63879907726df64e50da33f00d538759d789fe10fc5fbc95b'
-  head 'https://github.com/flexible-collision-library/fcl.git'
+  homepage "http://gamma.cs.unc.edu/FCL"
+  url "https://github.com/flexible-collision-library/fcl/archive/0.3.2.tar.gz"
+  sha256 "cf914f85b32cf8b63879907726df64e50da33f00d538759d789fe10fc5fbc95b"
+  head "https://github.com/flexible-collision-library/fcl.git"
 
-  depends_on 'boost'
-  depends_on 'cmake' => :build
-  depends_on 'pkg-config' => :build
-  depends_on 'homebrew/science/flann' => :optional
-  depends_on 'homebrew/science/libccd'
+  depends_on "boost"
+  depends_on "cmake' => :build
+  depends_on "pkg-config" => :build
+  depends_on "homebrew/science/flann" => :optional
+  depends_on "homebrew/science/libccd"
 
   def install
     system "cmake", ".", *std_cmake_args
