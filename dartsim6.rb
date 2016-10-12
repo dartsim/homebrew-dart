6cd13aa6afb7db88027cbfff603bab59cecb0420
Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
2016-10-12 09:12:21 -0400

    Add recommended dependency: "freeglut"

--- dartsim6.rb
+++ dartsim6.rb
@@ -30,6 +30,7 @@ class Dartsim6 < Formula
   depends_on "ros/deps/urdfdom" => :recommended
 
   # dart-gui
+  depends_on "freeglut" => :recommended
   depends_on "open-scene-graph" => :recommended
 
   def install
