203d2b397b6d33f3ada11d002319265ad42a0463
Jeongseok Lee <jslee02@gmail.com>
2017-08-07 15:08:47 -0700

    Bump version to 5.1.6

--- Formula/dartsim5.rb
+++ Formula/dartsim5.rb
@@ -1,8 +1,8 @@
 class Dartsim5 < Formula
   desc "DART: Dynamic Animation and Robotics Toolkit"
   homepage "https://dartsim.github.io"
-  url "https://github.com/dartsim/dart/archive/v5.1.5.tar.gz"
-  sha256 "8b55930ecdec99edabd88a62e3fe362e6104ccaa4bd0d5f68a2c1ef5bff2f0f4"
+  url "https://github.com/dartsim/dart/archive/v5.1.6.tar.gz"
+  sha256 "b3b04a321fed0e63483413cc7a9bea780a0b97b7baacf64a574cc042f612d1e3"
   head "https://github.com/dartsim/dart.git", :branch => "release-5.1"
 
   option "with-core-only", "Build dart-core only"
