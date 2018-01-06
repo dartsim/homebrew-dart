cd113df8e56ad531fa7f8a014600e54ebfafda5b
Jeongseok Lee <jslee02@gmail.com>
2018-01-06 06:48:03 -0800

    DART 4.3.7 (#50)

--- Formula/dartsim4.rb
+++ Formula/dartsim4.rb
@@ -1,8 +1,8 @@
 class Dartsim4 < Formula
   desc "DART: Dynamic Animation and Robotics Toolkit"
   homepage "https://dartsim.github.io"
-  url "https://github.com/dartsim/dart/archive/v4.3.6.tar.gz"
-  sha256 "12e1bac9f50d9f4057bd80ae20385216cfafb885c1ed10a0a69441e51d7729a9"
+  url "https://github.com/dartsim/dart/archive/v4.3.7.tar.gz"
+  sha256 "f98382c743194ce1e37e06e3002030efba6eb6819942eb665c3933df84789683"
   head "https://github.com/dartsim/dart.git", :branch => "release-4.3"
 
   option "with-core-only", "Build dart-core only"
@@ -20,7 +20,7 @@ class Dartsim4 < Formula
 
   depends_on "flann" if build.without? "core-only"
   depends_on "tinyxml" if build.without? "core-only"
-  depends_on "tinyxml2@3" if build.without? "core-only"
+  depends_on "tinyxml2" if build.without? "core-only"
   depends_on "ros/deps/urdfdom" if build.without? "core-only"
 
   conflicts_with "dartsim5", :because => "Differing version of the same formula"
@@ -34,6 +34,15 @@ class Dartsim4 < Formula
   end
 
   test do
-    system "false"
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <dart/dart.h>
+      int main() {
+        dart::simulation::World* world = new dart::simulation::World();
+        assert(world != NULL);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-I#{include}/eigen3", "-L#{lib}", "-ldart", "-lassimp", "-lc++", "-o", "test"
+    system "./test"
   end
 end
