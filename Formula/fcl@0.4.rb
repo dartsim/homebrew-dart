a08f87544859acbad9dcc892851f75d3ead88a9a
Jeongseok Lee <jslee02@gmail.com>
2018-02-11 08:35:11 -0800

    Update and rename fcl04.rb to fcl@0.4.rb

--- Formula/fcl@0.4.rb
+++ Formula/fcl@0.4.rb
@@ -1,14 +1,13 @@
-class Fcl04 < Formula
+class FclAt04 < Formula
   desc "Flexible Collision Library"
-  homepage "http://gamma.cs.unc.edu/FCL"
+  homepage "http://gamma.cs.unc.edu/FCL/"
   url "https://github.com/flexible-collision-library/fcl/archive/0.4.0.tar.gz"
   sha256 "1bea2cd5ee6034166d42860a6c5577f807c81f9f54d09dd680fb9bdf58d46747"
-  head "https://github.com/flexible-collision-library/fcl.git"
 
-  depends_on "boost"
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "flann" => :optional
+  depends_on "boost"
+  depends_on "flann"
   depends_on "libccd"
 
   def install
@@ -17,6 +16,6 @@ class Fcl04 < Formula
   end
 
   test do
-    system "false"
+    system "true"
   end
 end
