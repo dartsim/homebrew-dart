a6eeaac7c81f6fde72e3a77637ef8aa35e533fae
Jeongseok Lee <jslee02@gmail.com>
2018-02-11 08:19:03 -0800

    Update and rename dartsim6.rb to dartsim@6.rb

--- Formula/dartsim@6.rb
+++ Formula/dartsim@6.rb
@@ -1,9 +1,8 @@
-class Dartsim6 < Formula
+class DartsimAT6 < Formula
   desc "DART: Dynamic Animation and Robotics Toolkit"
-  homepage "https://dartsim.github.io"
+  homepage "https://dartsim.github.io/"
   url "https://github.com/dartsim/dart/archive/v6.3.0.tar.gz"
   sha256 "aa92634c1c97d99966cf16c4a0845792941358c063409fa00c28b4039c961c25"
-  head "https://github.com/dartsim/dart.git", :branch => "release-6.3"
 
   bottle do
     root_url "https://dl.bintray.com/dartsim/dart"
