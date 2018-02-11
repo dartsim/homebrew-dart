fa68f89422b727f908e61f2696de976dc3436474
Jeongseok Lee <jslee02@gmail.com>
2018-02-11 08:17:22 -0800

    Use formulae in homebrew-core

--- Formula/dartsim4.rb
+++ Formula/dartsim4.rb
@@ -1,9 +1,8 @@
 class Dartsim4 < Formula
   desc "DART: Dynamic Animation and Robotics Toolkit"
-  homepage "https://dartsim.github.io"
+  homepage "https://dartsim.github.io/"
   url "https://github.com/dartsim/dart/archive/v4.3.7.tar.gz"
   sha256 "f98382c743194ce1e37e06e3002030efba6eb6819942eb665c3933df84789683"
-  head "https://github.com/dartsim/dart.git", :branch => "release-4.3"
 
   option "with-core-only", "Build dart-core only"
 
@@ -15,16 +14,17 @@ class Dartsim4 < Formula
   depends_on "assimp"
   depends_on "boost"
   depends_on "eigen"
-  depends_on "dartsim/dart/fcl"
+  depends_on "fcl"
   depends_on "libccd"
 
   depends_on "flann" if build.without? "core-only"
   depends_on "tinyxml" if build.without? "core-only"
   depends_on "tinyxml2" if build.without? "core-only"
-  depends_on "ros/deps/urdfdom" if build.without? "core-only"
+  depends_on "urdfdom" if build.without? "core-only"
 
   conflicts_with "dartsim5", :because => "Differing version of the same formula"
   conflicts_with "dartsim6", :because => "Differing version of the same formula"
+  conflicts_with "dartsim", :because => "Homebrew-core version of the same formula"
 
   def install
     cmake_args = std_cmake_args
@@ -42,7 +42,8 @@ class Dartsim4 < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.cpp", "-I#{include}/eigen3", "-L#{lib}", "-ldart", "-lassimp", "-lc++", "-o", "test"
+    system ENV.cxx, "test.cpp", "-I#{include}/eigen3", "-L#{lib}",
+                    "-ldart", "-lassimp", "-o", "test"
     system "./test"
   end
 end
