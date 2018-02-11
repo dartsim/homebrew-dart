7766f0c5b0d5276864b378caeab78f31bee235c2
Jeongseok Lee <jslee02@gmail.com>
2018-02-11 09:57:08 -0800

    Update formulae reflecting homebrew-core (#63)

--- Formula/dartsim5.rb
+++ Formula/dartsim5.rb
@@ -1,9 +1,8 @@
 class Dartsim5 < Formula
   desc "DART: Dynamic Animation and Robotics Toolkit"
-  homepage "https://dartsim.github.io"
+  homepage "https://dartsim.github.io/"
   url "https://github.com/dartsim/dart/archive/v5.1.6.tar.gz"
   sha256 "b3b04a321fed0e63483413cc7a9bea780a0b97b7baacf64a574cc042f612d1e3"
-  head "https://github.com/dartsim/dart.git", :branch => "release-5.1"
 
   option "with-core-only", "Build dart-core only"
 
@@ -15,7 +14,7 @@ class Dartsim5 < Formula
   depends_on "assimp"
   depends_on "boost"
   depends_on "eigen"
-  depends_on "dartsim/dart/fcl"
+  depends_on "fcl"
   depends_on "libccd"
 
   depends_on "bullet" => :optional
@@ -23,15 +22,18 @@ class Dartsim5 < Formula
   depends_on "flann" if build.without? "core-only"
   depends_on "tinyxml" if build.without? "core-only"
   depends_on "tinyxml2" if build.without? "core-only"
-  depends_on "ros/deps/urdfdom" if build.without? "core-only"
+  depends_on "urdfdom" if build.without? "core-only"
   depends_on "nlopt" if build.without? "core-only" => :optional
   depends_on "dartsim/dart/ipopt" if build.without? "core-only" => :optional
   depends_on "open-scene-graph" if build.without? "core-only" => :optional
 
   conflicts_with "dartsim4", :because => "Differing version of the same formula"
-  conflicts_with "dartsim6", :because => "Differing version of the same formula"
+  conflicts_with "dartsim", :because => "Homebrew-core version of the same formula"
+
+  needs :cxx11
 
   def install
+    ENV.cxx11
     cmake_args = std_cmake_args
     cmake_args << "-DBUILD_CORE_ONLY=True" if build.with? "core-only"
     system "cmake", ".", *cmake_args
@@ -39,7 +41,7 @@ class Dartsim5 < Formula
   end
 
   test do
-    (testpath/"test.cpp").write <<-EOS.undent
+    (testpath/"test.cpp").write <<~EOS
       #include <dart/dart-core.h>
       int main() {
         auto world = std::make_shared<dart::simulation::World>();
@@ -47,7 +49,9 @@ class Dartsim5 < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.cpp", "-I#{include}/eigen3", "-L#{lib}", "-ldart", "-lassimp", "-lc++", "-std=c++11", "-o", "test"
+    system ENV.cxx, "test.cpp", "-I#{Formula["eigen"].include}",
+                    "-I#{include}", "-L#{lib}", "-ldart",
+                    "-lassimp", "-std=c++11", "-o", "test"
     system "./test"
   end
 end
