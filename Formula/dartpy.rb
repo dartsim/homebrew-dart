fb5d686092b17b88a40b6b42322eb4cab30c90d8
Jeongseok Lee <jslee02@gmail.com>
2019-06-13 23:40:49 -0700

    Add dartpy.rb

---
+++ Formula/dartpy.rb
@@ -0,0 +1,36 @@
+class Dartpy < Formula
+  desc "Python APIs for Dynamic Animation and Robotics Toolkit"
+  homepage "https://dartsim.github.io/"
+  url "https://github.com/dartsim/dart/archive/v6.9.1.tar.gz"
+  sha256 "9e2954aaf2d4538a7e4aab5188f1061c3e216c8c0b72483c2d0b9e814525acd1"
+  version "0.0.1"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "assimp"
+  depends_on "boost"
+  depends_on "bullet"
+  depends_on "eigen"
+  depends_on "fcl"
+  depends_on "flann"
+  depends_on "ipopt"
+  depends_on "libccd"
+  depends_on "nlopt"
+  depends_on "ode"
+  depends_on "open-scene-graph"
+  depends_on "tinyxml2"
+  depends_on "urdfdom"
+
+  def install
+    ENV.cxx11
+
+    # Force to link to system GLUT (see: https://cmake.org/Bug/view.php?id=16045)
+    system "cmake", ".", "-DGLUT_glut_LIBRARY=/System/Library/Frameworks/GLUT.framework",
+                         "-DDART_BUILD_DARTPY", *std_cmake_args
+    system "make", "dartpy", "install"
+  end
+
+  test do
+    system python, "-c", "import dartpy"
+  end
+end
